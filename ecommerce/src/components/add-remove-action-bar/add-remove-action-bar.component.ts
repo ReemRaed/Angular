bbb8069540ad4e63d4b38986901779b69046a294
Reem Raed <rmtaha@iti.gov.eg>
2024-12-26 11:13:39 +0200

    product

--- ecommerce/src/components/add-remove-action-bar/add-remove-action-bar.component.ts
+++ ecommerce/src/components/add-remove-action-bar/add-remove-action-bar.component.ts
@@ -1,16 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-remove-action-bar',
   templateUrl: './add-remove-action-bar.component.html',
-  styleUrls: ['./add-remove-action-bar.component.scss']
+  styleUrls: ['./add-remove-action-bar.component.scss'],
+
 })
 export class AddRemoveActionBarComponent {
   @Input() itemsNumber: number = 1;
   value: number = 1;
   disableBlock: boolean = false;
   img: string = "";
+  isVisible:boolean=true;
 
+  constructor(private elementRef: ElementRef)
+  {}
+
+  @HostListener('document:click',['$event'])
+  onClickOutside(event: MouseEvent): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.closeComponent();
+    }
+  }
+
+  closeComponent(): void {
+    this.isVisible = false; // Close or hide the component
+  }
   increment() {
     if (this.value < this.itemsNumber) {
       this.value++;
@@ -58,3 +74,4 @@ export class AddRemoveActionBarComponent {
 
 
 }
+
--- ecommerce/src/components/product/product.component.ts
+++ ecommerce/src/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
 import product from '../../Models/product';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { AddRemoveActionBarComponent } from "../add-remove-action-bar/add-remove-action-bar.component";
@@ -12,11 +12,21 @@ import { AddRemoveActionBarComponent } from "../add-remove-action-bar/add-remove
 export class ProductComponent {
   @Input() product!: product;
   disabled: boolean = true;
-
-
+  showActionBar = false; 
   list: string[] = ["free delivery", "only 18 tablets", "selling out fast", "only two left in stock"];
   currentWord: string = '';
   currentIndex: number = 0;
+
+  constructor(private elementRef: ElementRef) {}
+
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent): void {
+    if (this.showActionBar && !this.elementRef.nativeElement.contains(event.target)) {
+      this.showActionBar = false;
+    }
+  }
+
 
   ngOnInit() {
     setInterval(() => {
@@ -27,5 +37,7 @@ export class ProductComponent {
   }
   addToCart() {
     this.disabled = false;
+    this.showActionBar=true;
   }
+
 }
